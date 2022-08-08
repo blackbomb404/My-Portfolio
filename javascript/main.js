ab497b13dc4abf55b410c0f0b34158e3b7632370
Leonel <leonel-nascimento@hotmail.com>
2022-08-08 02:38:13 +0100

    Finished testimonials and contact form.

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,5 @@
 const filterOptions = document.querySelector('.projects .filter-options');
+let carouselInstance;
 
 filterOptions.addEventListener('click', e => {
     const t = e.target;
@@ -8,11 +9,39 @@ filterOptions.addEventListener('click', e => {
     }
 })
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
-  
-var instance = M.Carousel.init({
-    fullWidth: true,
-    indicators: true
+    const carousel = document.querySelector('.carousel');
+    carouselInstance = M.Carousel.init(carousel, {
+        fullWidth: true,
+        indicators: true,
+        noWrap: true,
+        // onCycleTo: resizeTab
+    });
+    
+    // resizeTab();
 });
+const backToTop = document.querySelector('.back-to-top');
+window.addEventListener('scroll', e => {
+    if(window.scrollY >= 2048)
+        backToTop.classList.add('show');
+    else
+        backToTop.classList.remove('show');
+    
+})
+backToTop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
+
+// function resizeTab(){
+//     let maxHeight = 0;
+//     const carouselItems = document.querySelectorAll('.carousel-item');
+
+//     let currentItemHeight;
+//     carouselItems.forEach(item => {
+//         currentItemHeight = getComputedStyle(item).height;
+//         currentItemHeight = currentItemHeight.substring(0, currentItemHeight.indexOf('px'));
+//         if(currentItemHeight > maxHeight)
+//             maxHeight = parseInt(currentItemHeight); 
+//     });
+
+//     document.querySelector('.carousel').style.height = maxHeight + 'px';
+// }
